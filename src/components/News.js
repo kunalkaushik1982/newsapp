cc2bd121db85d0b50b9fc5d9ffcd6655f20a3883
Kunal Kaushik <kunkaush1@publicisgroupe.net>
2022-03-27 20:41:30 +0530

    Refactoring News component to use the same function for loading news

--- src/components/News.js
+++ src/components/News.js
@@ -25,7 +25,7 @@ export class News extends Component {
         page:1
     }
   }
-  async componentDidMount(){
+  async updateNews(){
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d25c7176f766428eb050cfb4a0a77ebc&page=1&pageSize=${this.props.pageSize}`
     this.setState({loading:true})
     let data = await fetch(url)
@@ -37,31 +37,18 @@ export class News extends Component {
       loading:false
     })
   }
+  async componentDidMount(){
+    this.updateNews()
+  }
   handlePrevClick = async () =>{
       console.log("Previous")
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d25c7176f766428eb050cfb4a0a77ebc&page=${this.state.page-1}&pageSize=${this.props.pageSize}`
-      this.setState({loading:true})
-      let data = await fetch(url)
-      let parsedData = await data.json()
-      this.setState({
-        page:this.state.page-1,
-        articles:parsedData.articles,
-        loading:false
-      })
+      this.setState({page:this.setState.page-1})
+      this.updateNews()
   }
   handleNextClick = async () =>{
-    console.log("Next")
-    if(!(this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d25c7176f766428eb050cfb4a0a77ebc&page=${this.state.page+1}&pageSize=${this.props.pageSize}`
-      this.setState({loading:true})
-      let data = await fetch(url)
-      let parsedData = await data.json()
-      this.setState({
-        page:this.state.page+1,
-        articles:parsedData.articles,
-        loading:false
-      })
-    }    
+    console.log("Next") 
+    this.setState({page:this.setState.page+1})
+    this.updateNews()
   }
   render() {
     return (
