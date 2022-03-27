704e3d1eacc7704dec001e01e6a3a2d4a50247d5
Kunal Kaushik <kunkaush1@publicisgroupe.net>
2022-03-27 19:12:59 +0530

    Fetching News category wise

--- src/components/News.js
+++ src/components/News.js
@@ -9,15 +9,11 @@ export class News extends Component {
       country:"in",
       pageSize:6,
       category:"general"
-
-
     }
-    static propTypes = {
-      
+    static propTypes = {      
       country: PropTypes.string,
       pageSize: PropTypes.number,
-      category:PropTypes.string
-      
+      category:PropTypes.string      
     }
 
   constructor(){
@@ -29,18 +25,18 @@ export class News extends Component {
         page:1
     }
   }
-
   async componentDidMount(){
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d25c7176f766428eb050cfb4a0a77ebc&page=1&pageSize=${this.props.pageSize}`
     this.setState({loading:true})
     let data = await fetch(url)
     let parsedData = await data.json()
     console.log(parsedData)
-    this.setState({articles:parsedData.articles,
+    this.setState({
+      articles:parsedData.articles,
       totalResults:parsedData.totalResults,
-      loading:false})
+      loading:false
+    })
   }
-
   handlePrevClick = async () =>{
       console.log("Previous")
       let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d25c7176f766428eb050cfb4a0a77ebc&page=${this.state.page-1}&pageSize=${this.props.pageSize}`
@@ -53,7 +49,6 @@ export class News extends Component {
         loading:false
       })
   }
-
   handleNextClick = async () =>{
     console.log("Next")
     if(!(this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
@@ -66,21 +61,19 @@ export class News extends Component {
         articles:parsedData.articles,
         loading:false
       })
-
-    }
-    
+    }    
   }
-
   render() {
     return (
         <div className="container my-3">
           <h1 className="text-center" style={{margin: '35px 0px'}}>NewsApp - Top Headlines</h1>
           {this.state.loading && <Spinner/>}       
+          {/* http://ajaxloadingimages.net/ */}
           <div className="row">
           {!this.state.loading && this.state.articles.map((element)=>{
                   return <div className="col-md-4" key={element.url}>
                       <NewsItem title={element.title?element.title.slice(0,15):"MISSING Title"} description={element.description?element.description.slice(0,30):"Missing Description"} 
-                      imageurl={element.urlToImage} newsUrl={element.url}/>                
+                      imageurl={element.urlToImage?element.urlToImage:"https://img1.hscicdn.com/image/upload/f_auto/lsci/db/PICTURES/CMS/282100/282129.6.jpg"} newsUrl={element.url}/>                
                   </div>
               })}
           </div>
