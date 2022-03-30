c482dc20ddb90e7dd1d80d68f5157d8d3172e026
Kunal Kaushik <kunkaush1@publicisgroupe.net>
2022-03-30 19:04:16 +0530

    Adding a top loading bar & fix css of news source in NewsApp

--- src/App.js
+++ src/App.js
@@ -4,24 +4,35 @@ import React, { Component } from "react";
 import NavBar from './components/NavBar';
 import News from './components/News';
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
 export default class App extends Component {
   pageSize = 6
   country = "in"
   apiKey=process.env.REACT_APP_NEWS_API
+  state = {
+    progress:0
+  }
+  setProgress = (progress) =>{
+    this.setState({progress: progress})
+  }
   render() {
     return (
     <div>
       <Router>
-        <NavBar/>       
+        <NavBar/> 
+        <LoadingBar
+        color='#f11946'
+        progress={this.state.progress}
+      />      
         <Routes>key=""
-          <Route exact path="/" element={<News key="general" apiKey={this.apiKey} pageSize={this.pageSize} country={this.country} category="general"/>}/>
-          <Route exact path="/business" element={<News  key="business" apiKey={this.apiKey} pageSize={this.pageSize} country={this.country} category="business"/>}/>
-          <Route exact path="/entertainment" element={<News  key="entertainment" apiKey={this.apiKey} pageSize={this.pageSize} country={this.country} category="entertainment"/>}/>
-          <Route exact path="/general" element={<News  key="general" apiKey={this.apiKey} pageSize={this.pageSize} country={this.country} category="general"/>}/>
-          <Route exact path="/health" element={<News  key="health" apiKey={this.apiKey} pageSize={this.pageSize} country={this.country} category="health"/>}/>
-          <Route exact path="/science" element={<News  key="science" apiKey={this.apiKey} pageSize={this.pageSize} country={this.country} category="science"/>}/>
-          <Route exact path="/sports" element={<News  key="sports" apiKey={this.apiKey} pageSize={this.pageSize} country={this.country} category="sports"/>}/>
-          <Route exact path="/technology" element={<News  key="technology"apiKey={this.apiKey} pageSize={this.pageSize} country={this.country} category="technology"/>}/>
+          <Route exact path="/" element={<News setProgress={this.setProgress}  key="general" apiKey={this.apiKey} pageSize={this.pageSize} country={this.country} category="general"/>}/>
+          <Route exact path="/business" element={<News setProgress={this.setProgress}   key="business" apiKey={this.apiKey} pageSize={this.pageSize} country={this.country} category="business"/>}/>
+          <Route exact path="/entertainment" element={<News setProgress={this.setProgress}   key="entertainment" apiKey={this.apiKey} pageSize={this.pageSize} country={this.country} category="entertainment"/>}/>
+          <Route exact path="/general" element={<News setProgress={this.setProgress}   key="general" apiKey={this.apiKey} pageSize={this.pageSize} country={this.country} category="general"/>}/>
+          <Route exact path="/health" element={<News setProgress={this.setProgress}   key="health" apiKey={this.apiKey} pageSize={this.pageSize} country={this.country} category="health"/>}/>
+          <Route exact path="/science" element={<News setProgress={this.setProgress}   key="science" apiKey={this.apiKey} pageSize={this.pageSize} country={this.country} category="science"/>}/>
+          <Route exact path="/sports" element={<News setProgress={this.setProgress}   key="sports" apiKey={this.apiKey} pageSize={this.pageSize} country={this.country} category="sports"/>}/>
+          <Route exact path="/technology" element={<News setProgress={this.setProgress}   key="technology"apiKey={this.apiKey} pageSize={this.pageSize} country={this.country} category="technology"/>}/>
         </Routes>
       </Router>
     </div>
